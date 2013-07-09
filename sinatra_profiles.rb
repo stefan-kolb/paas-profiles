363e9858514fba086ac0ad68e0c860135472c374
stefan-kolb <stefan-kolb@web.de>
2013-07-09 15:54:33 +0200

    Layout changes

--- sinatra_profiles.rb
+++ sinatra_profiles.rb
@@ -1,9 +1,12 @@
 require 'sinatra'
 
 get '/vendors' do
+	@title = "Overview | PaaS Profiles"
 	erb :vendors
 end
 
 get '/vendor/:name' do
-  erb :vendor
+	vendor = :name
+  @title = "#{params[:name].gsub("_", " ").split(" ").map(&:capitalize).join(" ")} | PaaS Profiles"
+	erb :vendor
 end
