c92fc102d17c9991792ad3cf8b250e43cd5cf239
stefan-kolb <stefan-kolb@web.de>
2013-07-29 15:03:26 +0200

    Heroku stuff

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,6 +1,7 @@
 require 'rake'
 require 'json'
 require 'mongo'
+require 'uri'
 
 include Mongo
 
@@ -12,32 +13,51 @@ mongo = {
 	"db" => "paas"
 }
 
-unless ENV['VCAP_SERVICES'].nil?
-    env = JSON.parse(ENV['VCAP_SERVICES'])
-    mongo = env['mongodb-1.8'][0]['credentials']
-end
-
 namespace :mongo do
 		
     desc "Imports all of the specified JSON files to the Mongo collection"
     task :import do
-		
-			client = MongoClient.new(mongo["hostname"], mongo["port"])
-			db = client.db(mongo['db'])
-			unless mongo["hostname"] == "localhost"
-				db.authenticate(mongo["username"], mongo["password"])
-			end
-			col = db["vendors"]
-			# delete collection
-			col.remove()
-			
-			Dir.glob("profiles#{File::SEPARATOR}*.json").each do |file_name|
-
-				data = JSON.parse(File.read(file_name))
+			if ENV['MONGOHQ_URL'].nil?
+				client = MongoClient.new(mongo["hostname"], mongo["port"])
+				db = client.db(mongo['db'])
+				unless mongo["hostname"] == "localhost"
+					db.authenticate(mongo["username"], mongo["password"])
+				end
 				
-				col.save(data)
+				col = db["vendors"]
+				# delete collection
+				col.remove()
+				
+				Dir.glob("profiles#{File::SEPARATOR}*.json").each do |file_name|
+
+					data = JSON.parse(File.read(file_name))
+					
+					col.save(data)
+				end
+			else
+				db = get_connection
+
+				col = db["vendors"]
+				# delete collection
+				col.remove()
+				
+				Dir.glob("profiles#{File::SEPARATOR}*.json").each do |file_name|
+
+					data = JSON.parse(File.read(file_name))
+					
+					col.save(data)
+				end				
 			end
 		end
+end
+
+def get_connection
+  return @db_connection if @db_connection
+  db = URI.parse(ENV['MONGOHQ_URL'])
+  db_name = db.path.gsub(/^\//, '')
+  @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+  @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+  @db_connection
 end
 
 Rake::Task["mongo:import"].invoke
