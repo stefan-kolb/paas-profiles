5166c55d37912ab7625535454ca58380e163bd9b
stefan-kolb <stefan-kolb@web.de>
2013-09-04 12:57:04 +0200

    Minor changes

--- test/test_profile_integrity.rb
+++ test/test_profile_integrity.rb
@@ -8,9 +8,9 @@ require_relative '../models/vendor'
 class TestProfileIntegrity < Minitest::Test
 	# create a test for every profile
 	Dir.glob(File.dirname(__FILE__) + '/../profiles/*.json') do |file|
-		testname = File.basename(file, ".json")
+		filename = File.basename(file, ".json")
 		
-		define_method("test_#{testname}_profile") do
+		define_method("test_#{filename}_profile") do
 			profile = nil
 			# wellformedness
 			begin
@@ -19,11 +19,12 @@ class TestProfileIntegrity < Minitest::Test
 				assert(profile != nil, "JSON structure is not wellformed")
 			end
 			
-			# name must match filename removing spaces dashes etc
-			
 			# profile validation
 			obj = Vendor.new(profile)
 			assert(obj.valid?, obj.errors.full_messages) # todo get embedded messages
+			# name must match filename removing spaces dashes etc
+			exp_name = obj["name"].downcase.gsub(/[^a-z0-9]/, '_')
+			assert_equal(exp_name, filename, "Filename must match lowercase vendor name without all characters except a-z0-9 replaced by _")
 		end
 	end
 end
