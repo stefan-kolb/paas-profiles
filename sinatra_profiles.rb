ce3209cf6442098a6f1757588809e4417ab587e0
stefan-kolb <stefan-kolb@web.de>
2013-09-05 17:41:25 +0200

    Vendor verification
    Added verifications for vendors I had correspondence with.

--- sinatra_profiles.rb
+++ sinatra_profiles.rb
@@ -1,5 +1,6 @@
 require_relative 'models/vendor'
 
+require 'json'
 require 'mongoid'
 require 'sinatra'
 require 'newrelic_rpm'
@@ -11,18 +12,18 @@ get '/' do
 end
 
 get '/vendors' do
-	@title = "Platform as a Service Profiles"
+	@title = "Platform as a Service Provider Comparison"
 	erb :vendors
 end
 
 get '/vendor/:name' do
 	vendor = :name
-  @title = "#{params[:name].gsub("_", " ").split(" ").map(&:capitalize).join(" ")} | PaaS Profiles"
+  @title = "#{params[:name].gsub("_", " ").split(" ").map(&:capitalize).join(" ")} | PaaS Comparison"
 	erb :vendor
 end
 
 get '/filter' do
-	@title = "Find your PaaS | PaaS Profiles"
+	@title = "Find your PaaS | PaaS Comparison"
 	erb :filter
 end
 
