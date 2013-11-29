1f3cf3d302e6f50c1163172bff8acc0556a7b683
stefan-kolb <stefan-kolb@web.de>
2013-11-29 15:00:26 +0100

    Tweaked test generation

--- test/test_profile_integrity.rb
+++ test/test_profile_integrity.rb
@@ -1,24 +1,33 @@
-﻿gem 'minitest', '~> 4.7'
+﻿require 'minitest/autorun'
 require 'json'
-require 'minitest/autorun'
 require 'active_support/core_ext'
 
 require_relative '../models/vendor'
 
 module PaasProfiles
-# create a test for every profile
+  # create a test class for every profile
   Dir.glob(File.dirname(__FILE__) + '/../profiles/*.json') do |file|
-    new_class = Class.new(MiniTest::Unit::TestCase) do
-      @@file = file
+    # filename
+    filename = File.basename(file, ".json")
+
+    # test class
+    test_class = Class.new(MiniTest::Unit::TestCase) do
+      # set file path
+      @filepath = file
 
       attr_accessor :profile
 
+      def self.filepath
+        @filepath
+      end
+
+      # loads and validates profile
       def setup
         begin
-          @profile = JSON.parse(File.read(@@file))
+          json = JSON.parse(File.read(self.class.filepath))
+          @profile = Vendor.new(json)
           # profile validation
-          @profile = Vendor.new(profile)
-          assert(profile.valid?, profile.errors.full_messages) # todo get embedded messages
+          assert(@profile.valid?, profile.errors.full_messages)
         rescue JSON::ParserError
           assert(@profile != nil, "JSON structure is not wellformed")
         end
@@ -28,37 +37,35 @@ module PaasProfiles
         @profile = nil
       end
 
+      # filename must match lowercase vendor name after replacing [^a-z0-9] with '_'
       define_method("test_filename") do
-        # name must match filename removing spaces dashes etc
-        filename = File.basename(@@file, ".json")
         exp_name = profile["name"].downcase.gsub(/[^a-z0-9]/, '_')
         assert_equal(exp_name, filename, "Filename must match lowercase vendor name without all characters except a-z0-9 replaced by '_'")
       end
 
-      # no duplicates
+      # no runtime (language) duplicates
       define_method("test_runtime_duplicates") do
-        # check for runtime (language) duplicates
         uniq_runtimes = profile.runtimes.uniq { |item| item['language'] }
 
         assert_equal(uniq_runtimes.length, profile.runtimes.length, "There must be no duplicate runtime entries in one profile. Merge them into one entity")
       end
 
+      # no middleware (name) duplicates
       define_method("test_middleware_duplicates") do
-        # check for middleware (name) duplicates
         uniq_middleware = profile.middlewares.uniq { |item| item['name'] }
 
         assert_equal(uniq_middleware.length, profile.middlewares.length, "There must be no duplicate middleware entries in one profile. Merge them into one entity")
       end
 
+      # no framework (name) duplicates
       define_method("test_frameworks_duplicates") do
-        # check for frameworks (name) duplicates
         uniq_frameworks = profile.frameworks.uniq { |item| item['name'] }
 
         assert_equal(uniq_frameworks.length, profile.frameworks.length, "There must be no duplicate framework entries in one profile. Merge them into one entity")
       end
 
+      # no native services (name) duplicates
       define_method("test_native_services_duplicates") do
-        # check for native services (name) duplicates
         unless profile.service.blank? || profile.service.natives.blank?
           uniq_services = profile.service.natives.uniq { |item| item['name'] }
 
@@ -66,8 +73,8 @@ module PaasProfiles
         end
       end
 
+      # no add-on services (name) duplicates
       define_method("test_addon_services_duplicates") do
-        # check for add-on services (name) duplicates
         unless profile.service.blank? || profile.service.addons.blank?
           uniq_services = profile.service.addons.uniq { |item| item['name'] }
 
@@ -75,27 +82,43 @@ module PaasProfiles
         end
       end
 
+      # no compliance duplicates
       define_method("test_compliance_duplicates") do
-        # check for compliance duplicates
         uniq_compliance = profile.compliance.uniq
 
         assert_equal(uniq_compliance.length, profile.compliance.length, "There must be no duplicate compliance entries in one profile. Remove duplicate values")
       end
 
+      # no hosting duplicates
       define_method("test_hosting_duplicates") do
-        # check for hosting duplicates
         uniq_hosting = profile.hosting.uniq
 
         assert_equal(uniq_hosting.length, profile.hosting.length, "There must be no duplicate hosting entries in one profile. Remove duplicate values")
       end
 
+      # middleware runtime must be present
+      define_method("test_middleware_runtime_existence") do
+        profile.middlewares.each do |m|
+          unless m['runtime'].blank?
+            assert(profile.runtimes.any? { |r| r['language'] == m['runtime'] }, "Referenced middleware runtime must be defined under runtimes")
+          end
+        end
+      end
+
+      # framework runtime must be present
+      define_method("test_frameworks_runtime_existence") do
+        profile.frameworks.each do |f|
+          assert(profile.runtimes.any? { |r| r['language'] == f['runtime'] }, "Referenced framework runtime must be defined under runtimes")
+        end
+      end
+
       # TODO duplicate versions
-    end # classdef
+      # TODO version formats
+      # TODO service types
+    end
 
     # self-descriptive classname
-    filename = File.basename(file, ".json")
-    self.const_set("Test#{filename.capitalize}", new_class)
+    self.const_set("Test#{filename.capitalize}", test_class)
 
-  end # dir glob
-
+  end
 end
