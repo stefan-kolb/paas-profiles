265d93a8d261e294a052692f2ac766915409c214
stefan-kolb <stefan-kolb@web.de>
2013-12-10 15:14:02 +0100

    Adjusted navigation

--- config/navigation.rb
+++ config/navigation.rb
@@ -3,6 +3,11 @@ SimpleNavigation::Configuration.run do |navigation|
     primary.item :index, 'PaaS Profiles', '/vendors' do |index|
       index.item :vendor, @paas, @route
       index.item :filter, 'Find your PaaS', '/filter'
+      index.item :statistics, 'Statistics', '/statistics' do |statistics|
+        statistics.item :languages, 'Runtime Languages', '/statistics/languages'
+        statistics.item :services, 'Native Services', '/statistics/services'
+        statistics.item :status, 'Status', '/statistics/status'
+      end
     end
   end
 end
