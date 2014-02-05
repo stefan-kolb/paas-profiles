0e0c19fab90f931807d75390dce777f047b7086b
stefan-kolb <stefan-kolb@web.de>
2014-02-05 13:33:46 +0100

    Include statistics helper

---
+++ lib/helper/statistics_helper.rb
@@ -0,0 +1,32 @@
+require 'active_support/core_ext'
+
+module StatisticsHelper
+  def median(set)
+    if set.blank?
+      raise ArgumentError, 'There is no median for an empty set'
+    end
+
+    set.sort!
+    (set[(set.length - 1) / 2] + set[set.length / 2]) / 2.0
+  end
+
+  def mode(set)
+    if set.blank?
+      raise ArgumentError, 'There is no mode for an empty set'
+    end
+
+    freq = set.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+    uniq = set.uniq
+    uniq.delete_if { |e| freq[e] < freq.values.max }
+
+    uniq.size == 1 ? uniq.first : uniq
+  end
+
+  def mean(set)
+    if set.blank?
+      raise ArgumentError, 'There is no mean for an empty set'
+    end
+
+    (set.reduce(:+) / set.length.to_f).round(1)
+  end
+end
