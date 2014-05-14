b5aa445e8274c4fa1c05b64077f02be4d8a72d7c
stefan-kolb <stefan-kolb@web.de>
2014-05-14 14:29:07 +0200

    Revert to RSS feed

--- lib/feed.rb
+++ lib/feed.rb
@@ -4,11 +4,9 @@ require_relative '../models/vendor/vendor'
 
 class Feed
   def self.vendor_updates
-    rss = RSS::Maker.make('atom') do |maker|
+    rss = RSS::Maker.make('2.0') do |maker|
       maker.channel.title = 'PaaSify.it - Platform as a Service Providers'
       maker.channel.link = 'http://paasify.it'
-      maker.channel.author = 'PaaSify.it'
-      maker.channel.id = '0dfb6de0-f100-4a11-88c8-7fdc0bd5b44a'
       maker.channel.description = 'Platform as a Service provider overview, comparison and matchmaking.'
       maker.channel.updated = Time.now.to_s(:rfc822)
       maker.items.do_sort = true
@@ -16,13 +14,11 @@ class Feed
       Vendor.all.each do |vendor|
         maker.items.new_item do |item|
           item.id = url_encode(vendor.name) << vendor.revision.to_s
-          #item.guid.isPermaLink =false
+          item.guid.isPermaLink =false
           item.link = "http://paasify.it/vendor/#{url_encode(vendor.name)}"
           item.title = "PaaSify Update #{vendor.revision.strftime('%-m/%-d')}: #{vendor.name}"
           item.updated = vendor.revision.to_s(:rfc822)
         end
-
-
       end
     end
 
