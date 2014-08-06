9a00ef1296938ab44fc4c6635c3f1aa99e481303
stefan-kolb <stefan-kolb@web.de>
2014-08-06 12:19:06 +0200

    Add a test and missing vendor images

---
+++ test/test_images.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+
+class TestImages < MiniTest::Test
+  # for every vendor a small and a big logo must be available
+  def test_vendor_logos
+    Dir.glob(File.dirname(__FILE__) + '/../profiles/*.json') do |file|
+      vendor = File.basename(file, '.json')
+      assert(File.exist?('../public/img/vendor/' + vendor + '.png'), "No small logo available for #{vendor}")
+      assert(File.exist?('../public/img/vendor/' + vendor  + '_big.png'), "No big logo available for #{vendor}")
+    end
+  end
+end
