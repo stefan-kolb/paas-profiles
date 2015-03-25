209d78c29afeb37da7e5963899081e1dcbe018cf
stefan-kolb <stefan-kolb@web.de>
2015-03-25 18:18:28 +0100

    Add new service vendor model

---
+++ app/models/service_vendor.rb
@@ -0,0 +1,16 @@
+module Profiles
+  class ServiceVendor
+    include Mongoid::Document
+
+    field :key, type: String
+    field :name, type: String
+    field :url, type: String
+    field :twitter, type: String
+    field :description, type: String
+    field :type, type: String
+    # validations
+    validates :name, presence: true
+    validates :url, allow_blank: true, format: {with: %r{http[s]?://.*}}
+    validates :description, allow_blank: true, format: {with: %r{\A.{,140}\z}}
+  end
+end
