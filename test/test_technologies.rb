e89421805ce518cd829d8eff62fdabd1257a1830
Stefan Kolb <stefan-kolb@web.de>
2016-06-09 12:14:09 +0200

    Rubocop

--- test/test_technologies.rb
+++ test/test_technologies.rb
@@ -9,7 +9,7 @@ module Profiles
       technologies = JSON.parse(File.read(File.join(__dir__, '..', 'data', 'technologies.json')))
 
       Dir.glob(profile_dir) do |file|
-        vendor = File.basename(file, '.json').gsub('_', ' ')
+        vendor = File.basename(file, '.json').tr('_', ' ')
 
         assert(technologies.detect { |t| t['vendor'].casecmp(vendor) }, "No technology data available for #{vendor}")
       end
