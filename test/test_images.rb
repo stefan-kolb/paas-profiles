2a97b0d01e65073df2ecf420fc6d6874238692e1
Stefan Kolb <stefan-kolb@web.de>
2016-08-08 13:28:02 +0200

    Remove unused vendor images

--- test/test_images.rb
+++ test/test_images.rb
@@ -2,18 +2,32 @@ require 'minitest/autorun'
 
 module Profiles
   class TestImages < MiniTest::Test
-    # for every vendor a small and a big logo must be available
-    def test_vendor_logos
-      profile_dir = File.join(__dir__, '..', 'profiles', '*.json')
-      img_dir = File.join(__dir__, '..', 'public', 'img', 'vendor')
+    PROFILE_DIR = File.join(__dir__, '..', 'profiles')
+    IMAGE_DIR = File.join(__dir__, '..', 'public', 'img', 'vendor')
 
-      Dir.glob(profile_dir) do |file|
+    # for every vendor a small and a big logo must be available
+    def test_logo_for_profile
+      Dir.glob(File.join(PROFILE_DIR, '*.json')) do |file|
         vendor = File.basename(file, '.json')
-        default_img = File.join(img_dir, "#{vendor}.png")
-        big_img = File.join(img_dir, "#{vendor}_big.png")
+        default_img = File.join(IMAGE_DIR, "#{vendor}.png")
+        big_img = File.join(IMAGE_DIR, "#{vendor}_big.png")
 
         assert(File.exist?(default_img), "No small logo available for #{vendor}")
         assert(File.exist?(big_img), "No big logo available for #{vendor}")
+      end
+    end
+
+    def test_profile_for_logo
+      Dir.glob(File.join(IMAGE_DIR, '*.png')) do |file|
+        vendor = if File.basename(file, '.png').end_with? 'big'
+                   File.basename(file, '.png').gsub('_big', '')
+                 else
+                   File.basename(file, '.png')
+                 end
+
+        profile = File.join(PROFILE_DIR, "#{vendor}.json")
+
+        assert(File.exist?(profile), "No profile available for #{vendor}")
       end
     end
   end
